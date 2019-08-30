a1bd797e6469b0335605713ae53e04c41af71a62
Fernando Marinho <fernando.marinho@uconn.edu>
2019-08-30 01:17:10 -0400

    JS - Computer generated number works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,21 +3,16 @@
 
 
 //$(document).ready(function() {
-
-    var computerNumber = randomNumber;
-    var yourScore = "";
+    var computerNumber = (Math.floor(Math.random() * (120 - 19 + 1) + 19));
+    var yourScore = "9";
     var crystalRed = "";
     var crystalBlue = "";
     var crystalPurple = "";
     var crystalGreen = "";
 
 
-function randomNumber() {
-   19 < (Math.random()) < 120;
-}
-
-randomNumber();
-console.log(randomNumber);
+//WORKS Spits the number chosen by the computer 
+$("#generatedNumber").html(computerNumber);
 
 //What happens when you click on the crystals
 
