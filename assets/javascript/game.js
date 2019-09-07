eedd3fa4d0d088435bd046b8128bc1047886f432
Fernando Marinho <fernando.marinho@uconn.edu>
2019-09-06 23:45:55 -0400

    JS in progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,40 +15,35 @@ $(document).ready(function() {
     //WORKS Spits the number chosen by the computer 
 $("#generatedNumber").html("The Master Crystal's number is " + generatedNumber );
 
-
- // var crystalRed = 12;
- // var crystalGreen = 8;
-//var crystalPurple = 6;
-  //var crystalBlue = 1;
-  //var yourSum = 0;
-
   $("#ruby").click(function () {
-    $("#yourTotal").innerHTML = 
+    document.getElementById("yourTotal").innerHTML = 
     yourScore += crystalRed;
     console.log(yourScore);
     //isWinner();
 });
 
 $("#emerald").click(function () {
-  $("#yourTotal").innerHTML = 
+  document.getElementById("yourTotal").innerHTML = 
   yourScore += crystalGreen;
   console.log(yourScore);
   //isWinner();
 });
 
 $("#sapphire").click(function () {
-  $("#yourTotal").innerHTML = 
+  document.getElementById("yourTotal").innerHTML = 
   yourScore += crystalPurple;
   console.log(yourScore);
   //isWinner();
 });
 
 $("#quartz").click(function () {
-  $("#yourTotal").innerHTML = 
+  document.getElementById("yourTotal").innerHTML = 
   yourScore += crystalBlue;
   console.log(yourScore);
   //isWinner();
 });
+
+
     //$("#rainbow").click(function () {
     //    document.getElementById("result").innerHTML = 
     //    sum += rainbowNumber;
@@ -59,39 +54,6 @@ $("#crystalsYouHave").html("The number of crystals you have so far is:" );
 
 
 });
-
-
-
-//What happens when you click on the crystals
-//Example of on click - $( "p" ).click(function() { $( this ).slideUp();});
-
-  //$("#ruby").click( console.log(addingRuby) );
-
-
-  //$("#emerald").click(function addingEmerald() {
-  //   console.log(yourSum+=crystalGreen);
-  // });
-
-  //$("#sapphire").click(function addingSapphire() {
-  //  console.log(yourSum+=crystalPurple);
-  //});
-
-  //$("#quartz").click(function addingQuartz(){
-  //console.log(yourSum+=crystalBlue);
-  //});
-
-//This Variable is the sum of all crystals
-
-//var finalSum = addingRuby() + addingEmerald() + addingSapphire() + addingQuartz() ;
-//console.log(finalSum);
-
-  
-
- 
-  //}
-
-
-
 
   //PSEUDOCODE
   //Page generates a ramdom number
