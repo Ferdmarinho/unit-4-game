c967be665d212d6b5cd57add32b4a7149cbb9f61
Fernando Marinho <fernando.marinho@uconn.edu>
2019-09-06 23:08:11 -0400

    JS in progress, CSS final, HTML adjusted according to JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,92 @@
+//This will wrap all to make it work once the page is loaded
+$(document).ready(function() {
 
-//$(document).ready(function() {
-  var computerNumber = (Math.floor(Math.random() * (120 - 19 + 1) + 19));
-  var yourScore = 0;
-  var crystalRed = 12;
-  var crystalGreen = 8;
-  var crystalPurple = 6;
-  var crystalBlue = 1;
-  var yourSum = 0;
-  var finalSum = yourSum;
+//Generated Number
+  var generatedNumber = (Math.floor(Math.random() * (120 - 19 + 1) + 19));
 
-//WORKS Spits the number chosen by the computer 
-$("#generatedNumber").html("The wizard's number is " + computerNumber );
+//Crystals Numbers 
+    var crystalRed = 12;
+    var crystalGreen = 8;
+    var crystalPurple = 6;
+    var crystalBlue = 1;
+    
+    var yourScore = 0;
+
+    //WORKS Spits the number chosen by the computer 
+$("#generatedNumber").html("The Master Crystal's number is " + generatedNumber );
+
+
+ // var crystalRed = 12;
+ // var crystalGreen = 8;
+//var crystalPurple = 6;
+  //var crystalBlue = 1;
+  //var yourSum = 0;
+
+  $("#ruby").click(function () {
+    $("#yourTotal").innerHTML = 
+    yourScore += crystalRed;
+    console.log(yourScore);
+    //isWinner();
+});
+
+$("#emerald").click(function () {
+  $("#yourTotal").innerHTML = 
+  yourScore += crystalGreen;
+  console.log(yourScore);
+  //isWinner();
+});
+
+$("#sapphire").click(function () {
+  $("#yourTotal").innerHTML = 
+  yourScore += crystalPurple;
+  console.log(yourScore);
+  //isWinner();
+});
+
+$("#quartz").click(function () {
+  $("#yourTotal").innerHTML = 
+  yourScore += crystalBlue;
+  console.log(yourScore);
+  //isWinner();
+});
+    //$("#rainbow").click(function () {
+    //    document.getElementById("result").innerHTML = 
+    //    sum += rainbowNumber;
+    //    console.log(sum)
+    //    isWinner();
+
+$("#crystalsYouHave").html("The number of crystals you have so far is:" );
+
+
+});
+
+
 
 //What happens when you click on the crystals
 //Example of on click - $( "p" ).click(function() { $( this ).slideUp();});
 
-  $("#ruby").click(function addingRuby() {
-    (yourSum+=crystalRed);
-  });
+  //$("#ruby").click( console.log(addingRuby) );
 
-  $("#emerald").click(function addingEmerald() {
-    (yourSum+=crystalGreen);
-  });
 
-  $("#sapphire").click(function addingSapphire() {
-    (yourSum+=crystalPurple);
-  });
+  //$("#emerald").click(function addingEmerald() {
+  //   console.log(yourSum+=crystalGreen);
+  // });
 
-  $("#quartz").click(function addingQuartz(){
-    (yourSum+=crystalBlue);
-  });
+  //$("#sapphire").click(function addingSapphire() {
+  //  console.log(yourSum+=crystalPurple);
+  //});
+
+  //$("#quartz").click(function addingQuartz(){
+  //console.log(yourSum+=crystalBlue);
+  //});
 
 //This Variable is the sum of all crystals
-var finalSum = (addingRuby());
 
-console.log(finalSum);
+//var finalSum = addingRuby() + addingEmerald() + addingSapphire() + addingQuartz() ;
+//console.log(finalSum);
 
-  $("#scoreNumber").html("The number of crystals you have so far is  " + yourSum );
   
-  if (computerNumber = finalSum) {
-    $("#winnerMessage").html("You won one!");
-  }
-  
+
  
   //}
 
